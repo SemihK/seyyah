315ea65b2834df2063b98bdb1c5d727507a6977b
Semih Kesgin <SemihK@users.noreply.github.com>
2024-08-31 18:10:48 +0300

    project setup

---
+++ seyyah/seyyah/seyyahApp.swift
@@ -0,0 +1,17 @@
+//
+//  seyyahApp.swift
+//  seyyah
+//
+//  Created by semihkesgin on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct seyyahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

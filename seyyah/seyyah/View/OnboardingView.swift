c879d73489c3b9091d5ab64dfd4261c7bcb3cd89
Semih Kesgin <SemihK@users.noreply.github.com>
2024-08-31 18:21:04 +0300

    MVVM Setup

---
+++ seyyah/seyyah/View/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  seyyah
+//
+//  Created by semihkesgin on 31.08.2024.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
